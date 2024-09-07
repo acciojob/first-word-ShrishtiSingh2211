840d4c1e85ee292a093b38208b7662f02f199e01
ShrishtiSingh2211 <142424324+ShrishtiSingh2211@users.noreply.github.com>
2024-09-07 19:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-function firstWord(s) {
-  // your code here
-}
+function firstWord(str) {
+  // Trim leading and trailing whitespaces
+  str = str.trim();
+  
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
 
+  // If no space is found, return the whole string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Return the substring from the start to the first space
+  return str.substring(0, spaceIndex);
+}
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+const str = prompt("Enter String:");
+alert(firstWord(str));
